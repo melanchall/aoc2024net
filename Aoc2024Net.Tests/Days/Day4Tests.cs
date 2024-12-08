0827038bd13dca30f602e87edf152a01ab3d14be
Maxim K. Dobroselsky <m.hall.muz@gmail.com>
2024-12-08 16:38:42 +0300

    Day 4 solved

--- Aoc2024Net.Tests/Days/Day4Tests.cs
+++ Aoc2024Net.Tests/Days/Day4Tests.cs
@@ -3,8 +3,33 @@ using NUnit.Framework;
 
 namespace Aoc2024Net.Tests.Days
 {
-    [DayDataPart1(null)]
-    [DayDataPart2(null)]
+    [DayDataPart1(@"..X...
+                    .SAMX.
+                    .A..A.
+                    XMAS.S
+                    .X....", 4)]
+    [DayDataPart1(@"MMMSXXMASM
+                    MSAMXMSMSA
+                    AMXSXMAAMM
+                    MSAMASMSMX
+                    XMASAMXAMM
+                    XXAMMXXAMA
+                    SMSMSASXSS
+                    SAXAMASAAA
+                    MAMMMXMMMM
+                    MXMXAXMASX", 18)]
+    [DayDataPart1(2401)]
+    [DayDataPart2(@".M.S......
+                    ..A..MSMS.
+                    .M.S.MAA..
+                    ..A.ASMSM.
+                    .M.S.M....
+                    ..........
+                    S.S.S.S.S.
+                    .A.A.A.A..
+                    M.M.M.M.M.
+                    ..........", 9)]
+    [DayDataPart2(1822)]
     [TestFixture]
     public sealed class Day4Tests : DayTests<Day4Tests>
     {
--- Aoc2024Net/Days/Day4.cs
+++ Aoc2024Net/Days/Day4.cs
@@ -2,8 +2,56 @@
 {
     internal sealed class Day4 : Day
     {
-        public override object? SolvePart1() => null;
+        public override object? SolvePart1()
+        {
+            const int margin = 3;
 
-        public override object? SolvePart2() => null;
+            var (grid, width, height) = InputData.GetInputCharGrid(margin, ' ');
+            var result = 0;
+
+            static int IsXmas(char x, char m, char a, char s) =>
+                Convert.ToInt32($"{x}{m}{a}{s}" == "XMAS");
+
+            for (var x = margin; x < width + margin; x++)
+            {
+                for (var y = margin; y < height + margin; y++)
+                {
+                    result +=
+                        IsXmas(grid[x, y], grid[x + 1, y], grid[x + 2, y], grid[x + 3, y]) +
+                        IsXmas(grid[x, y], grid[x - 1, y], grid[x - 2, y], grid[x - 3, y]) +
+                        IsXmas(grid[x, y], grid[x, y + 1], grid[x, y + 2], grid[x, y + 3]) +
+                        IsXmas(grid[x, y], grid[x, y - 1], grid[x, y - 2], grid[x, y - 3]) +
+                        IsXmas(grid[x, y], grid[x + 1, y + 1], grid[x + 2, y + 2], grid[x + 3, y + 3]) +
+                        IsXmas(grid[x, y], grid[x + 1, y - 1], grid[x + 2, y - 2], grid[x + 3, y - 3]) +
+                        IsXmas(grid[x, y], grid[x - 1, y - 1], grid[x - 2, y - 2], grid[x - 3, y - 3]) +
+                        IsXmas(grid[x, y], grid[x - 1, y + 1], grid[x - 2, y + 2], grid[x - 3, y + 3]);
+                }
+            }
+
+            return result;
+        }
+
+        public override object? SolvePart2()
+        {
+            const int margin = 2;
+
+            var (grid, width, height) = InputData.GetInputCharGrid(margin, ' ');
+            var result = 0;
+
+            static int IsXmas(char leftTop, char rightTop, char middle, char leftBottom, char rightBottom) =>
+                Convert.ToInt32(
+                    ($"{leftTop}{middle}{rightBottom}" == "MAS" || $"{leftTop}{middle}{rightBottom}" == "SAM") &&
+                    ($"{rightTop}{middle}{leftBottom}" == "MAS" || $"{rightTop}{middle}{leftBottom}" == "SAM"));
+
+            for (var x = margin; x < width + margin; x++)
+            {
+                for (var y = margin; y < height + margin; y++)
+                {
+                    result += IsXmas(grid[x, y], grid[x + 2, y], grid[x + 1, y + 1], grid[x, y + 2], grid[x + 2, y + 2]);
+                }
+            }
+
+            return result;
+        }
     }
 }
