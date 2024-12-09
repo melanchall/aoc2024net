c86b3493f30c2682313bf47df428de9d9c3e0b76
Maxim K. Dobroselsky <m.hall.muz@gmail.com>
2024-12-09 18:33:19 +0300

    Day 7 solved

--- Aoc2024Net.Tests/Days/Day7Tests.cs
+++ Aoc2024Net.Tests/Days/Day7Tests.cs
@@ -3,8 +3,26 @@ using NUnit.Framework;
 
 namespace Aoc2024Net.Tests.Days
 {
-    [DayDataPart1(null)]
-    [DayDataPart2(null)]
+    [DayDataPart1(@"190: 10 19
+                    3267: 81 40 27
+                    83: 17 5
+                    156: 15 6
+                    7290: 6 8 6 15
+                    161011: 16 10 13
+                    192: 17 8 14
+                    21037: 9 7 18 13
+                    292: 11 6 16 20", 3749)]
+    [DayDataPart1(10741443549536)]
+    [DayDataPart2(@"190: 10 19
+                    3267: 81 40 27
+                    83: 17 5
+                    156: 15 6
+                    7290: 6 8 6 15
+                    161011: 16 10 13
+                    192: 17 8 14
+                    21037: 9 7 18 13
+                    292: 11 6 16 20", 11387)]
+    [DayDataPart2(500335179214836)]
     [TestFixture]
     public sealed class Day7Tests : DayTests<Day7Tests>
     {
--- Aoc2024Net/Days/Day7.cs
+++ Aoc2024Net/Days/Day7.cs
@@ -2,8 +2,73 @@
 {
     internal sealed class Day7 : Day
     {
-        public override object? SolvePart1() => null;
+        private enum Operator
+        {
+            Add,
+            Multiply,
+            Concatenate,
+        }
 
-        public override object? SolvePart2() => null;
+        private static readonly Operator[] SimpleOperatorsSet = [Operator.Add, Operator.Multiply];
+        private static readonly Operator[] ExtendedOperatorsSet = [Operator.Add, Operator.Multiply, Operator.Concatenate];
+
+        public override object? SolvePart1() =>
+            Solve(false);
+
+        public override object? SolvePart2() =>
+            Solve(true);
+
+        private long Solve(bool useExtendedOperatorsSet)
+        {
+            var equations = InputData
+                .GetInputLines()
+                .Select(l => l.Split(':', StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries))
+                .Select(p => (long.Parse(p[0]), p[1].Split(' ', StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries).Select(int.Parse).ToArray()))
+                .ToArray();
+
+            var result = 0L;
+            var cache = new Operator[equations.Max(e => e.Item2.Length)][][];
+
+            foreach (var (testValue, numbers) in equations)
+            {
+                var operatorsSets = GetOperatorsSets(numbers.Length - 1, cache, useExtendedOperatorsSet).ToArray();
+
+                foreach (var set in operatorsSets)
+                {
+                    var value = new[] { Operator.Add }
+                        .Concat(set)
+                        .Zip(numbers, (o, n) => (o, n))
+                        .Aggregate(0L, (res, x) => x.o == Operator.Add ? res + x.n : (x.o == Operator.Concatenate ? res * (long)Math.Pow(10, x.n.ToString().Length) + x.n : res * x.n));
+
+                    if (value == testValue)
+                    {
+                        result += testValue;
+                        break;
+                    }
+                }
+            }
+
+            return result;
+        }
+
+        private static Operator[][] GetOperatorsSets(int places, Operator[][][] cache, bool useExtendedOperatorsSet)
+        {
+            var operatorsSet = useExtendedOperatorsSet
+                ? ExtendedOperatorsSet
+                : SimpleOperatorsSet;
+
+            if (places == 1)
+                return operatorsSet.Select(s => new[] { s }).ToArray();
+
+            if (cache[places] != null)
+                return cache[places];
+
+            return cache[places] = operatorsSet
+                .SelectMany(op => GetOperatorsSets(places - 1, cache, useExtendedOperatorsSet)
+                    .Select(s => new[] { op }
+                    .Concat(s)
+                    .ToArray()))
+                .ToArray();
+        }
     }
 }
