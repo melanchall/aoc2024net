4fe6f0b793d61574cca49eb920e5a387fb95430f
Maxim K. Dobroselsky <m.hall.muz@gmail.com>
2024-12-19 19:05:36 +0300

    Day 19 solved

--- Aoc2024Net.Tests/Days/Day19Tests.cs
+++ Aoc2024Net.Tests/Days/Day19Tests.cs
@@ -3,8 +3,28 @@ using NUnit.Framework;
 
 namespace Aoc2024Net.Tests.Days
 {
-    [DayDataPart1(null)]
-    [DayDataPart2(null)]
+    [DayDataPart1(@"r, wr, b, g, bwu, rb, gb, br
+
+                    brwrr
+                    bggr
+                    gbbr
+                    rrbgbr
+                    ubwu
+                    bwurrg
+                    brgr
+                    bbrgwb", 6)]
+    [DayDataPart1(304)]
+    [DayDataPart2(@"r, wr, b, g, bwu, rb, gb, br
+
+                    brwrr
+                    bggr
+                    gbbr
+                    rrbgbr
+                    ubwu
+                    bwurrg
+                    brgr
+                    bbrgwb", 16)]
+    [DayDataPart2(705756472327497)]
     [TestFixture]
     public sealed class Day19Tests : DayTests<Day19Tests>
     {
--- Aoc2024Net/Days/Day19.cs
+++ Aoc2024Net/Days/Day19.cs
@@ -2,8 +2,52 @@
 {
     internal sealed class Day19 : Day
     {
-        public override object? SolvePart1() => null;
+        public override object? SolvePart1() =>
+            GetArrangementCounts().Count;
 
-        public override object? SolvePart2() => null;
+        public override object? SolvePart2() =>
+            GetArrangementCounts().Sum(c => c.Value);
+
+        private Dictionary<string, long> GetArrangementCounts()
+        {
+            var linesGroups = InputData.GetInputLinesGroups();
+
+            var towels = linesGroups[0][0].Split(',', StringSplitOptions.TrimEntries).ToArray();
+            var designs = linesGroups[1];
+
+            var counts = new Dictionary<string, long>();
+
+            foreach (var design in designs)
+            {
+                GetArrangementCounts(design, towels, counts);
+            }
+
+            return counts
+                .Where(c => designs.Contains(c.Key) && c.Value > 0)
+                .ToDictionary(
+                    c => c.Key,
+                    c => c.Value);
+        }
+
+        private static long GetArrangementCounts(
+            string design,
+            string[] towels,
+            Dictionary<string, long> counts)
+        {
+            if (counts.TryGetValue(design, out var c))
+                return c;
+
+            var result = towels.LongCount(t => t == design);
+
+            foreach (var t in towels)
+            {
+                if (!design.StartsWith(t))
+                    continue;
+
+                result += GetArrangementCounts(design[t.Length..], towels, counts);
+            }
+
+            return counts[design] = result;
+        }
     }
 }
